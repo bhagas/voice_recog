042d18b039e53df9c05c88d822221cf659b53aa0
unknown <tump.store@gmail.com>
2022-05-17 11:42:48 +0700

    training

---
+++ latihan.py
@@ -0,0 +1,175 @@
+import librosa
+#matplotlib inline
+import matplotlib.pyplot as plt
+import librosa.display
+from IPython.display import Audio
+import numpy as np
+import tensorflow as tf
+from matplotlib.pyplot import specgram
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import IPython.display as ipd  
+import os 
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
+from tensorflow.keras.layers import Input, Flatten, Dropout, Activation, BatchNormalization, Dense
+from sklearn.model_selection import GridSearchCV
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.regularizers import l2
+import seaborn as sns
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report
+
+#----labeling
+audio = "./dataset/"
+actor_folders = os.listdir(audio) #list file di directory
+actor_folders.sort() 
+actor_folders[0:5]
+
+emotion = []
+gender = []
+actor = []
+file_path = []
+for i in actor_folders:
+    filename = os.listdir(audio + i) #iterate aktor tiap folder
+    for f in filename: # masuk ke folder
+        part = f.split('.')[0].split('-')
+        emotion.append(int(part[2]))
+        actor.append(int(part[6]))
+        bg = int(part[6])
+        if bg%2 == 0:
+            bg = "perempuan"
+        else:
+            bg = "laki-laki"
+        gender.append(bg)
+        file_path.append(audio + i + '/' + f)
+
+audio_df = pd.DataFrame(emotion)
+audio_df = audio_df.replace({1:'netral', 2:'calm', 3:'bahagia', 4:'sedih', 5:'marah', 6:'takut', 7:'jijik', 8:'terkejut'})
+audio_df = pd.concat([pd.DataFrame(gender),audio_df,pd.DataFrame(actor)],axis=1)
+audio_df.columns = ['gender','emotion','actor']
+audio_df = pd.concat([audio_df,pd.DataFrame(file_path, columns = ['path'])],axis=1)
+audio_df
+
+audio_df.to_csv('./label.csv')
+
+
+df = pd.DataFrame(columns=['mel_spectrogram'])
+
+counter=0
+
+for index,path in enumerate(audio_df.path):
+    X, sample_rate = librosa.load(path, res_type='kaiser_fast',duration=3,sr=44100,offset=0.5)
+    
+    #get the mel-scaled spectrogram (ransform both the y-axis (frequency) to log scale, and the “color” axis (amplitude) to Decibels, which is kinda the log scale of amplitudes.)
+    spectrogram = librosa.feature.melspectrogram(y=X, sr=sample_rate, n_mels=128,fmax=8000) 
+    db_spec = librosa.power_to_db(spectrogram)
+    #temporally average spectrogram
+    log_spectrogram = np.mean(db_spec, axis = 0)
+
+    
+    df.loc[counter] = [log_spectrogram]
+    counter=counter+1   
+
+print(len(df))
+df.head()
+
+
+df_combined = pd.concat([audio_df,pd.DataFrame(df['mel_spectrogram'].values.tolist())],axis=1)
+df_combined = df_combined.fillna(0)
+
+df_combined.drop(columns='path',inplace=True)
+
+# TRAIN TEST SPLIT DATA
+train,test = train_test_split(df_combined, test_size=0.2, random_state=0,
+                               stratify=df_combined[['emotion','gender','actor']])
+X_train = train.iloc[:, 3:]
+y_train = train.iloc[:,:2].drop(columns=['gender'])
+print(X_train.shape)
+
+X_test = test.iloc[:,3:]
+y_test = test.iloc[:,:2].drop(columns=['gender'])
+print(X_test.shape)
+
+# NORMALIZE DATA
+mean = np.mean(X_train, axis=0)
+std = np.std(X_train, axis=0)
+X_train = (X_train - mean)/std
+X_test = (X_test - mean)/std
+
+# masukkan data ke array untuk keras
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+lb = LabelEncoder()
+y_train = to_categorical(lb.fit_transform(y_train))
+y_test = to_categorical(lb.fit_transform(y_test))
+
+print(y_test[0:10])
+print(lb.classes_)
+
+# RESHAPE DATA KE 3D TENSOR 
+X_train = X_train[:,:,np.newaxis]
+X_test = X_test[:,:,np.newaxis]
+
+X_train.shape
+
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential, load_model
+
+#BUAT CNN LAYER
+model = Sequential()
+model.add(Conv1D(64, kernel_size=(20), activation='relu', input_shape=(X_train.shape[1],1)))
+model.add(Conv1D(128, kernel_size=(20),activation='relu',kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
+model.add(MaxPooling1D(pool_size=(8)))
+model.add(Dropout(0.4))
+model.add(Conv1D(128, kernel_size=(20),activation='relu'))
+model.add(MaxPooling1D(pool_size=(8)))
+model.add(Dropout(0.4))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(8, activation='softmax'))
+opt = keras.optimizers.Adam(lr=0.0001)
+model.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'])
+model.summary()
+
+# TARINING MODEL DAN SIMPAN MODEL (TERBAIK)
+checkpoint = ModelCheckpoint("hasil_latihan.hdf5", monitor='val_accuracy', verbose=1,
+    save_best_only=True, mode='max', period=1, save_weights_only=True)
+
+model_history=model.fit(X_train, y_train,batch_size=32, epochs=40, validation_data=(X_test, y_test),callbacks=[checkpoint])
+
+# TAMPILKAN
+plt.plot(model_history.history['accuracy'])
+plt.plot(model_history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig('Initial_Model_Accuracy.png')
+plt.show()
+# LOSSNYA
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('Initial_Model_loss.png')
+plt.show()
